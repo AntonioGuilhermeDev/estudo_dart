d31c30a080f53e2b6bd2b97889ec515a99f41f3e
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-02-12 19:29:56 -0300

    Operadores Aritmeticos

---
+++ Basico/operadores_aritmeticos.dart
@@ -0,0 +1,29 @@
+void main() {
+  double n1 = 10.5;
+  double n2 = 47.9;
+  int n3 = 15;
+  int n4 = 32;
+
+  double result1 = n1 + n2;
+  print(result1);
+  int result2 = n4 - n3;
+  print(result2);
+
+  double result3 = n1 * n4;
+  print(result3);
+
+  double result4 = n1 + n2 * n4 + (n1 + 40);
+  print(result4);
+
+  print(n4 % 3);
+
+  n1 += 10;
+  print(n1);
+
+  n1 -= 10;
+  print(n1);
+
+  n1++;
+  print(n1);
+
+}
