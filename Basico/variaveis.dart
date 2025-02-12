9b75682d785a2a5a73bd52e2791a4c1c0d3f6038
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-02-12 19:29:41 -0300

    Variaveis

---
+++ Basico/variaveis.dart
@@ -0,0 +1,15 @@
+void main() {
+  String nome = 'Guilherme';
+  int idade = 18;
+  double distancia = 24.98;
+  bool ligado = true;
+
+  print(nome);
+  print(idade);
+  print(distancia);
+  print(ligado);
+
+  idade = 19;
+
+  print(idade);
+}
