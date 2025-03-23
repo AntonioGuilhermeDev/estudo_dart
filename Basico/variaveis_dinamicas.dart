3884ba20768e3af60647d75635bd06c6399ba015
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-03-23 11:21:40 -0300

    Variaveis dinamicas

---
+++ Basico/variaveis_dinamicas.dart
@@ -0,0 +1,14 @@
+void main(){
+  var nome = 'Teste';
+  nome = "Guilherme";
+
+  var idade = 18;
+  idade + 2;
+
+  dynamic variavel = 'Guilherme';
+  variavel = 3;
+  variavel = true;
+
+  num number = 1.4;
+  number = 2;
+}
