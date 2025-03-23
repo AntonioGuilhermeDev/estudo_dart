b9da727f63585be850254dc336bc069e017f22be
Guilherme Sousa <antonioguilhermeslima@gmail.com>
2025-03-23 11:23:48 -0300

    Constantes e Finals

---
+++ Basico/final_const.dart
@@ -0,0 +1,7 @@
+void main(){
+  const String nome = 'Guilherme'; //Tempo de compilaçao
+  const int idade = 10 + 8;
+  const List<String> lista = ['Guilherme', 'Sousa'];
+
+  final DateTime now = DateTime.now(); //Tempo de execuçao
+}
